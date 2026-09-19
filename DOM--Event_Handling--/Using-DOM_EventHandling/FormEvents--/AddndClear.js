21d1fd2b33fbc2a230ee5ea7cbde262b1f253c3b
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-09-19 17:48:27 +0530

    Add & Clear form with error msg

---
+++ DOM--Event_Handling--/Using-DOM_EventHandling/FormEvents--/AddndClear.js
@@ -0,0 +1,54 @@
+let myForm = document.myfrm;
+let txtFirst = myForm.txtfno;
+let txtSec = myForm.txtsno;
+let txtThird = myForm.txtresult;
+let btnAdd = myForm.btnadd;
+let btnClear = myForm.btnclear;
+const span1 = document.querySelector("#fnoerror");
+const span2 = document.querySelector("#snoerror");
+
+const validate = (fnum, snum) => {
+	let isValid = true;
+	if (fnum.length === 0 || isNaN(fnum)) {
+		if (fnum.length === 0) {
+			span1.innerText = "Input Required";
+		} else {
+			span1.innerText = "Only digits allowed";
+		}
+		span1.style.color = "crimson";
+		txtFirst.classList.add("error");
+		isValid = false;
+	} else {
+		span1.innerText = "";
+		txtFirst.classList.remove("error");
+	}
+	if (snum.length === 0 || isNaN(snum)) {
+		if (snum.length === 0) {
+			span2.innerText = "Input Required";
+		} else {
+			span2.innerText = "Only digits allowed";
+		}
+		span2.style.color = "crimson";
+		txtSec.classList.add("error");
+		isValid = false;
+	} else {
+		span2.innerText = "";
+		txtSec.classList.remove("error");
+	}
+	return isValid;
+};
+btnAdd.addEventListener("click", () => {
+	let x = txtFirst.value;
+	let y = txtSec.value;
+	let result = validate(x, y);
+    if(result === true){
+    let z =  Number(x) + Number(y) ;
+    txtThird.value = z ;
+    }
+});
+btnClear.addEventListener("click" , ()=>{
+    txtFirst.value="";
+    txtSec.value="";
+    txtThird.value="";
+    txtFirst.focus();
+})
